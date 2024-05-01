2a99d01fadd44e981d62d292c5d47dc1ef7c8034
chi-chi-fan <free0nemeng@gmail.com>
2024-05-01 16:42:35 -0700

    0-2

---
+++ chapter0/EnterNumber/EnterNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{   
+    int fav_num;
+    std::cout << "Enter a number between 1 and 100: ";
+    std::cin >> fav_num;
+    std::cout << "Amazing!" << std::endl;
+
+    return 0;
+}
